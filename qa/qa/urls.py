1e802cbd15afd0275a1e9fc128ef4698ea60f1a8
Niral Patel <niral.patel65@gmail.com>
2013-07-07 18:13:43 -0400

    updated ship

--- qa/qa/urls.py
+++ qa/qa/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import patterns, include, url
+from django.conf.urls.static import static
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
@@ -7,14 +8,11 @@ admin.autodiscover()
 urlpatterns = patterns('',
     # Examples:
     url(r'^$', 'views.home'),
-    url(r'^qp/$', 'views.qp'),	    
-    url(r'^dashboard/$', 'views.start'),
-    url(r'^question/$', 'views.finished'),
-    url(r'^getQuestions/$','views.getQuestions'),
-    url(r'^interviewend/$','views.niralpage'),
+    url(r'^interview/$', 'views.start'),
+    url(r'^summary/$', 'views.summary'),
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
-)
+)   

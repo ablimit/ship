f001e3978b5696a28523867081c10df4e34db7a2
Niral Patel <niral.patel65@gmail.com>
2013-07-16 05:32:22 -0400

    updated files

--- qa/qa/urls.py
+++ qa/qa/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import patterns, include, url
 from django.conf.urls.static import static
-from views import InterviewQuestions
+#from views import InterviewQuestions
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
@@ -11,8 +11,8 @@ urlpatterns = patterns('',
     url(r'^$', 'views.home'),
     url(r'^login/$', 'views.login'),
     url(r'^userinfo/$', 'views.getuserinfo'),
-    url(r'^interview/$', InterviewQuestions.as_view()),
-    url(r'^summary/$', 'views.summary'),
+    url(r'^interview/$', 'views.generateInterview'),
+    url(r'^summary/$', 'views.evaluateInterview'),
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
